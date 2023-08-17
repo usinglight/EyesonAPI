80a97cb0e93caf670b2ac52ef85f97977e677c3c
Dinesh Shenoy <dinesh@airkit.com>
2023-08-17 13:03:20 +0200

    Added image and font resources and included sample config for creating overlay.

--- EyesonAPI/ec_client.test.py
+++ EyesonAPI/ec_client.test.py
@@ -4,8 +4,6 @@ import sys
 import requests
 from eyeson import EyesonClient
 
-BASE_URL = 'https://api.eyeson.team'
-
 
 def main(argv):
     parser = argparse.ArgumentParser(
@@ -17,13 +15,13 @@ def main(argv):
         current_room = json.load(f)
     access_key = current_room['access_key']
 
-    ec = EyesonClient.get_room(access_key, base_url=BASE_URL)
+    ec = EyesonClient.get_room(access_key)
     ec.broadcast_message('Hello World')
     # ec.change_layout(layout_type='auto', layout_name='six', users=['','','test_id','','',''])
     # ec.create_snapshot()
     # ec.get_room_details()
     # ec.image_overlay(url='https://eyeson-team.github.io/api/images/eyeson-overlay.png')
-    ec.local_image_overlay(filename='../resources/images/eyeson-overlay.png')
+    ec.local_image_overlay(filename='resources/images/eyeson-overlay.png')
     ec.playback(url='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4', )
 
 
