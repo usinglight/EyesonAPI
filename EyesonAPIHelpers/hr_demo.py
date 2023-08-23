1e1cba7d30d5985375703b105615131e915daf06
Stefan <stst@usinglight.com>
2023-08-23 15:07:42 +0200

    modified:   EyesonAPIHelpers/hr_demo.py Added latest module code and threw out about the code I had.

--- EyesonAPIHelpers/hr_demo.py
+++ EyesonAPIHelpers/hr_demo.py
@@ -2,43 +2,23 @@ import flet as ft
 import re
 import requests
 
+# When running on the local branch for testing
 import sys
 sys.path.append('/Users/stefansteinbauer/Github/EyesonAPI/eyeson/')
+
 import eyeson
 
 
 def main(page: ft.Page):
-    def get_access(s):
-        match = re.search(r'guest=(.*)', s)
-        if match:
-            guest_token = match.group(1)
-            url = "https://api.eyeson.team/guests/" + guest_token
-            headers = {
-                "Content-Type": "application/json"
-            }
-            data = {
-                "id": "Assistant", 
-                "name": "Assistant"
-            }
-
-            response = requests.post(url, headers=headers, json=data)
-            # If the response status code is 201 OK (user created), print the response JSON and save response
-            if response.status_code == 201:
-                return response.json()['access_key']
-            else:
-                return None
-
     def guest_add(e):
-        access_key = get_access(guest_link.value)
-        access_key =str(access_key)
-        if access_key:
-            view.controls.append(ft.TextField(value=access_key))
-            # how would I call the client ?
-            ec = eyeson.EyesonClient.get_room(access_key,base_url="https://api.eyeson.team")
-            ec.base_url = "https://api.eyeson.team"
-            ec.broadcast_message('Hello World')
+        if guest_link.value:
+            ec = eyeson.EyesonClient.register_guest(guest_link.value)
+            print("Room Details")
+            print(ec.room_details)
+            print("Access Key")
+            print(ec.access_key)
         else:
-            print("Error getting access key") 
+            print("error accessing call")
         view.update()
 
     guest_link = ft.TextField(hint_text="Paste your guest link here")
